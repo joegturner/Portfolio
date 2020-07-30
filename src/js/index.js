ada787175978dcd2e6023217cfb0600cd8512f15
joegturner <59180847+joegturner@users.noreply.github.com>
2020-07-30 18:55:44 -0400

    work on projects cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,21 @@
 const cards = document.getElementsByClassName("card");
 
 const body = document.getElementsByTagName("body")[0];
-console.log(cards);
-
-body.addEventListener("click", (event) => {
-  const rotated = document.getElementsByClassName("card__rotate")[0];
-  const target = event.target;
-
-  if (rotated !== target.parentElement) {
-    rotated.classList.remove("card__rotate");
-  }
-
-  //   console.log(event.target);
-});
 
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", (event) => {
-    const target = event.target.parentElement;
-
-    target.classList.add("card__rotate");
-    console.log(target.classList);
+    const card = event.target.closest(".card");
+    if (card) {
+      card.classList.add("card__rotate");
+    }
   });
 }
+
+body.addEventListener("click", (event) => {
+  const card = event.target.closest(".card");
+  const cardBack = event.target.closest(".card__side--back");
+
+  if (cardBack) {
+    card.classList.remove("card__rotate");
+  }
+});
