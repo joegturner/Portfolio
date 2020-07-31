c8875ab2f96c07e92cc07edfe84c22879cb7d395
joegturner <59180847+joegturner@users.noreply.github.com>
2020-07-30 22:44:40 -0400

    responsive fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-import smoothscroll from "smoothscroll-polyfill";
-
 const cards = document.getElementsByClassName("card");
 const body = document.getElementsByTagName("body")[0];
 const navButton = document.getElementsByClassName("nav__checkbox")[0];
@@ -8,8 +6,6 @@ const navButton = document.getElementsByClassName("nav__checkbox")[0];
 // - card rotation
 // - navigation menu close
 // - smooth scrolling
-
-smoothscroll.polyfill();
 
 // add rotate to card if touched
 for (let i = 0; i < cards.length; i++) {
@@ -21,13 +17,32 @@ for (let i = 0; i < cards.length; i++) {
   });
 }
 
+// smooth scroll to anchor tags on mobile
+function scrollTo(element) {
+  window.scroll({
+    behavior: "smooth",
+    left: 0,
+    top: element.offsetTop,
+  });
+}
+
 body.addEventListener("click", (event) => {
+  // PROJECT CARDS
   // flip project card back over
   const card = event.target.closest(".card");
   const cardBack = event.target.closest(".card__side--back");
 
   if (cardBack) {
     card.classList.remove("card__rotate");
+  }
+
+  // NAV BUTTON
+  // call smooth scroll to target anchor tag
+  const targetAnchor = document.getElementById(
+    event.target.innerHTML.toLowerCase()
+  );
+  if (targetAnchor) {
+    scrollTo(targetAnchor);
   }
 
   // close nav button pop up in mobile
