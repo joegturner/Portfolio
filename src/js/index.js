bf13a5fd8334f5f091557a1658110f33ab880bd3
joegturner <59180847+joegturner@users.noreply.github.com>
2020-07-30 21:20:43 -0400

    added remaining projects

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 const cards = document.getElementsByClassName("card");
-
 const body = document.getElementsByTagName("body")[0];
+const navButton = document.getElementsByClassName("nav__checkbox")[0];
 
+// Animations for mobile touch screens
+// - card rotation
+// - navigation menu close
+
+// add rotate to card if touched
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", (event) => {
     const card = event.target.closest(".card");
@@ -12,10 +17,20 @@ for (let i = 0; i < cards.length; i++) {
 }
 
 body.addEventListener("click", (event) => {
+  // flip project card back over
   const card = event.target.closest(".card");
   const cardBack = event.target.closest(".card__side--back");
 
   if (cardBack) {
     card.classList.remove("card__rotate");
+  }
+
+  // close nav button pop up in mobile
+  const navClicked =
+    event.target.parentElement.classList[0] === "nav-mobile__links" ||
+    event.target.parentElement.classList[0] === "nav-mobile__link";
+
+  if (navClicked) {
+    navButton.checked = false;
   }
 });
