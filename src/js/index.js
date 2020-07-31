3d99afc50dc0aa3b55b5baf6ab8b5252f4c117c8
joegturner <59180847+joegturner@users.noreply.github.com>
2020-07-31 10:24:35 -0400

    smooth scroll on mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const cards = document.getElementsByClassName("card");
 const body = document.getElementsByTagName("body")[0];
 const navButton = document.getElementsByClassName("nav__checkbox")[0];
+const navLinks = document.getElementsByClassName("nav-target");
 
 // Animations for mobile touch screens
 // - card rotation
@@ -16,9 +17,28 @@ for (let i = 0; i < cards.length; i++) {
   });
 }
 
+// add smooth scroll on nav buttons
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", (event) => {
+    let target = "";
+    if (event.target.closest("span") === null) {
+      target = event.target.firstElementChild.innerHTML.toLowerCase();
+    } else {
+      target = event.target.innerHTML.toLowerCase();
+    }
+
+    if (target) {
+      const anchor = document.getElementById(target);
+      anchor.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+}
+
 body.addEventListener("click", (event) => {
   // PROJECT CARDS
   // flip project card back over
+  // console.log(event.target.firstElementChild);
+
   const card = event.target.closest(".card");
   const cardBack = event.target.closest(".card__side--back");
 
